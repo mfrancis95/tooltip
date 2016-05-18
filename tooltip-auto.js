0396960db9b27647106ca762340bc50b50697599
mfrancis95 <mikefrancis95@gmail.com>
2016-05-18 11:13:13 -0400

    Move element looping code to tooltip-auto.js

---
+++ tooltip-auto.js
@@ -0,0 +1,4 @@
+var elements = document.getElementsByClassName("tooltipper");
+for (var i = 0; i < elements.length; i++) {
+    elements[i].tooltipify();
+}
--- tooltip.js
+++ tooltip.js
@@ -1,22 +1,20 @@
-var elements = document.getElementsByClassName("tooltipper");
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-    var dataset = element.dataset;
-    element.addEventListener("mouseenter", function() {
-        element.classList.add("tooltipper-hover");
+Element.prototype.tooltipify = function() {
+    var self = this;
+    var dataset = self.dataset;
+    self.addEventListener("mouseenter", function() {
+        self.classList.add("tooltipper-hover");
         setTimeout(function() {
-            if (element.classList.contains("tooltipper-hover")) {
+            if (self.classList.contains("tooltipper-hover")) {
                 var tooltip = document.createElement("span");
                 tooltip.classList.add("tooltip", "tooltip-show");
                 tooltip.innerHTML = dataset.text;
-                element.appendChild(tooltip);
+                self.appendChild(tooltip);
             }
         }, dataset.delay || 1000);
-
     });
-    element.addEventListener("mouseleave", function() {
-        element.classList.remove("tooltipper-hover");
-        var tooltips = element.getElementsByClassName("tooltip");
+    self.addEventListener("mouseleave", function() {
+        self.classList.remove("tooltipper-hover");
+        var tooltips = self.getElementsByClassName("tooltip");
         for (var i = 0; i < tooltips.length; i++) {
             var tooltip = tooltips[i];
             tooltip.classList.remove("tooltip-show");
@@ -24,4 +22,4 @@ for (var i = 0; i < elements.length; i++) {
             tooltip.addEventListener("animationend", tooltip.remove);
         }
     });
-}
+};
