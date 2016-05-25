7c5b3f772b2d52cac337d8240ec6be79e963fd58
mfrancis95 <mikefrancis95@gmail.com>
2016-05-25 16:48:50 -0400

    Improve mobile support

--- tooltip.js
+++ tooltip.js
@@ -1,25 +1,43 @@
 Element.prototype.tooltipify = function() {
     var self = this;
     var dataset = self.dataset;
-    self.addEventListener("mouseenter", function() {
+    var active = false;
+    var showTooltip = function() {
         self.classList.add("tooltipper-hover");
-        setTimeout(function() {
-            if (self.classList.contains("tooltipper-hover")) {
-                var tooltip = document.createElement("span");
-                tooltip.classList.add("tooltip", "tooltip-show");
-                tooltip.innerHTML = dataset.text;
-                self.appendChild(tooltip);
-            }
-        }, dataset.delay || 1000);
-    });
-    self.addEventListener("mouseleave", function() {
+        if (!active) {
+            active = true;
+            setTimeout(function () {
+                if (self.classList.contains("tooltipper-hover")) {
+                    var tooltip = document.createElement("span");
+                    tooltip.classList.add("tooltip", "tooltip-show");
+                    tooltip.innerHTML = dataset.text;
+                    self.appendChild(tooltip);
+                }
+                else {
+                    active = false;
+                }
+            }, dataset.delay || 1000);
+        }
+        
+    };
+    var hideTooltip = function() {
         self.classList.remove("tooltipper-hover");
-        var tooltips = self.getElementsByClassName("tooltip");
-        for (var i = 0; i < tooltips.length; i++) {
-            var tooltip = tooltips[i];
+        var tooltip = self.getElementsByClassName("tooltip")[0];
+        if (tooltip) {
             tooltip.classList.remove("tooltip-show");
             tooltip.classList.add("tooltip-hide");
-            tooltip.addEventListener("animationend", tooltip.remove);
+            tooltip.addEventListener("animationend", function() {
+                active = false;
+                tooltip.remove();
+                if (self.classList.contains("tooltipper-hover")) {
+                    showTooltip();
+                }
+            });
         }
+    };
+    document.addEventListener("click", function(event) {
+        (event.target === self ? showTooltip : hideTooltip)();
     });
+    self.addEventListener("mouseenter", showTooltip);
+    self.addEventListener("mouseleave", hideTooltip);
 };
