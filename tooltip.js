50414ca902e26517927edc3f4f708d54b1aa9398
mfrancis95 <mikefrancis95@gmail.com>
2016-06-01 11:04:35 -0400

    Fix duplicate tooltips

--- tooltip.js
+++ tooltip.js
@@ -2,28 +2,33 @@ Element.prototype.tooltipify = function() {
     var self = this;
     var dataset = self.dataset;
     var active = false;
+    var createTooltip = function() {
+        if (self.classList.contains("tooltipper-hover")) {
+            var tooltip = document.createElement("span");
+            tooltip.classList.add("tooltip", "tooltip-show");
+            tooltip.innerHTML = dataset.text;
+            self.appendChild(tooltip);
+        }
+        else {
+            active = false;
+        }
+    };
     var showTooltip = function() {
         self.classList.add("tooltipper-hover");
         if (!active) {
             active = true;
-            setTimeout(function () {
-                if (self.classList.contains("tooltipper-hover")) {
-                    var tooltip = document.createElement("span");
-                    tooltip.classList.add("tooltip", "tooltip-show");
-                    tooltip.innerHTML = dataset.text;
-                    self.appendChild(tooltip);
-                }
-                else {
-                    active = false;
-                }
-            }, dataset.delay || 1000);
+            setTimeout(createTooltip, dataset.delay || 1000);
         }
         
     };
     var hideTooltip = function() {
         self.classList.remove("tooltipper-hover");
-        var tooltip = self.getElementsByClassName("tooltip")[0];
+        var tooltips = self.getElementsByClassName("tooltip");
+        var tooltip = tooltips[0];
         if (tooltip) {
+            for (var i = 1; i < tooltips.length; i++) {
+                tooltips[i].remove();
+            }
             tooltip.classList.remove("tooltip-show");
             tooltip.classList.add("tooltip-hide");
             tooltip.addEventListener("animationend", function() {
