047282ae90cf3fcbe3c62d7df028a7bd5ca7cd8c
mfrancis95 <mfrancis95@users.noreply.github.com>
2026-04-09 21:30:17 -0400

    tooltip.js: Simplify logic by relying on `tooltipper-hover` class instead of `active` to determine if it should show a tooltip

--- tooltip.js
+++ tooltip.js
@@ -1,12 +1,8 @@
 Element.prototype.tooltipify = function() {
     const { delay = 1000 } = this.dataset;
     const text = this.dataset.text ?? this.ariaLabel;
-    let active = false;
     const createTooltip = () => {
-        if (this.querySelector('.tooltip')) {
-            active = false;
-        }
-        else {
+        if (!this.querySelector('.tooltip')) {
             const tooltip = document.createElement('span');
             tooltip.classList.add('tooltip', 'tooltip-show');
             tooltip.role = 'tooltip';
@@ -16,25 +12,16 @@ Element.prototype.tooltipify = function() {
     };
     const showTooltip = () => {
         this.classList.add('tooltipper-hover');
-        if (!active) {
-            active = true;
-            setTimeout(createTooltip, delay);
-        }
-        
+        setTimeout(createTooltip, delay);
     };
     const hideTooltip = () => {
         this.classList.remove('tooltipper-hover');
-        const tooltips = this.querySelectorAll('.tooltip');
-        const tooltip = tooltips[0];
+        const tooltip = this.querySelector('.tooltip');
         if (tooltip) {
-            for (let i = 1; i < tooltips.length; i++) {
-                tooltips[i].remove();
-            }
             tooltip.classList.remove('tooltip-show');
             tooltip.classList.add('tooltip-hide');
             tooltip.addEventListener('animationend', () => {
                 tooltip.remove();
-                active = false;
                 if (this.classList.contains('tooltipper-hover')) {
                     showTooltip();
                 }
